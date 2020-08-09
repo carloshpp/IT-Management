2847de6cef6080e61360055a1931ad37a22d265f
Carlos Henrique Pimentel Paiva <kaike_henrique_godim@hotmail.com>
2020-08-09 20:16:49 -0300

    create virtualenv

---
+++ ServiceManagement/env-servicemanagement/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Carlos\Source\Repos\IT-Management\ServiceManagement\env-servicemanagement\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
